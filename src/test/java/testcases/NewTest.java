17d3a80a5505ce4e8902a2c0eb2c1ec66b8cba88
User <User@DESKTOP-RV3G2MV>
2021-04-09 17:48:21 +0530

    ok

--- src/test/java/testcases/NewTest.java
+++ src/test/java/testcases/NewTest.java
@@ -1,11 +1,11 @@
-package testcases;
-
-import org.testng.annotations.Test;
-
-public class NewTest extends varstore{
-  @Test
-  public void f() {
-	  gvar="testing";
-	  
-  }
-}
+//package testcases;
+//
+//import org.testng.annotations.Test;
+//
+//public class NewTest extends varstore{
+//  @Test
+//  public void f() {
+//	  gvar="testing";
+//	  
+//  }
+//}
--- src/test/java/testcases/NewTest2.java
+++ src/test/java/testcases/NewTest2.java
@@ -1,11 +1,11 @@
-package testcases;
-
-import org.testng.annotations.Test;
-
-public class NewTest2 extends varstore{
-  @Test
-  public void f() {
-	  System.out.println(gvar);
-	  
-  }
-}
+//package testcases;
+//
+//import org.testng.annotations.Test;
+//
+//public class NewTest2 extends varstore{
+//  @Test
+//  public void f() {
+//	  System.out.println(gvar);
+//	  
+//  }
+//}
